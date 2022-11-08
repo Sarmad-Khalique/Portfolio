60a1d840a3dc3ec2c38f66f1c6ae4945aba5ae7c
Sarmad-Khalique <sarmadkhalique001@gmail.com>
2022-11-08 22:47:07 +0500

    Removed Anchor tags

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ const Navbar = ()=>{
         <div className={"flex items-center justify-between w-full px-5 py-5 bg-indigo-800"}>
             <div className={"w-fit text-2xl font-bold text-white"}>Sarmad<span>K.</span></div>
             <div className={`${open?"flex":"translate-x-[100%]"} transition-all duration-300 flex flex-col text-indigo-800 w-screen fixed items-center justify-around bg-white h-screen inset-0 md:relative md:transition-none md:w-auto md:h-auto md:translate-x-0 md:flex-row md:space-x-5 md:bg-transparent`}>
-                <a href={"#projects"} className={styles.link}>Home</a>
-                <a className={styles.link}>Projects</a>
-                <a className={styles.link}>Contact</a>
+                <div className={styles.link}>Home</div>
+                <div className={styles.link}>Projects</div>
+                <div className={styles.link}>Contact</div>
             </div>
             <BiMenuAltRight className={`z-10 md:hidden ${open?"text-indigo-800":"text-white"}`} onClick={()=>setOpen(!open)} />
         </div>
